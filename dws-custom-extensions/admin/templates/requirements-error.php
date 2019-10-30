744ea477043bccfed07321bbae8cb825328b8eb7
Fatine Tazi <f.tazi@deep-web-solutions.de>
2019-10-30 11:25:35 +0100

    Made requirements error translatable + added all empty index files to git

--- dws-custom-extensions/admin/templates/requirements-error.php
+++ dws-custom-extensions/admin/templates/requirements-error.php
@@ -4,7 +4,7 @@
  * A very early error message displayed if environment requirements are not met.
  *
  * @since   1.0.0
- * @version 1.0.0
+ * @version 2.0.0
  *
  * @see     dws_requirements_error
  */
@@ -13,25 +13,28 @@
 
 <div class="error">
     <p>
-		<?php echo DWS_CUSTOM_EXTENSIONS_NAME; ?> error: Your environment doesn't meet all of the system requirements
-        listed below.
+        <?php
+            echo sprintf(
+                __('%s error: Your environment doesn\'t meet all of the system requirements listed below.', DWS_CUSTOM_EXTENSIONS_LANG_DOMAIN),
+                DWS_CUSTOM_EXTENSIONS_NAME);
+        ?>
     </p>
 
     <ul class="ul-disc">
         <li>
             <strong>PHP <?php echo DWS_CUSTOM_EXTENSIONS_MIN_PHP; ?>+</strong>
-            <em>(You're running version <?php echo PHP_VERSION; ?>)</em>
+            <em><?php echo sprintf(__('You\'re running version %s', DWS_CUSTOM_EXTENSIONS_LANG_DOMAIN), PHP_VERSION); ?></em>
         </li>
 
         <li>
             <strong>WordPress <?php echo DWS_CUSTOM_EXTENSIONS_MIN_WP; ?>+</strong>
-            <em>(You're running version <?php echo esc_html($GLOBALS['wp_version']); ?>)</em>
+            <em><?php echo sprintf(__('You\'re running version %s', DWS_CUSTOM_EXTENSIONS_LANG_DOMAIN), esc_html($GLOBALS['wp_version'])); ?></em>
         </li>
     </ul>
 
     <p>
-        If you need to upgrade your version of PHP you can ask your hosting company for assistance,
-        and if you need help upgrading WordPress you can refer to <a
-                href="http://codex.wordpress.org/Upgrading_WordPress">the Codex</a>.
+        <?php
+            _e('If you need to upgrade your version of PHP you can ask your hosting company for assistance, and if you need help upgrading WordPress you can refer to <a href="http://codex.wordpress.org/Upgrading_WordPress">the Codex</a>.', DWS_CUSTOM_EXTENSIONS_LANG_DOMAIN);
+        ?>
     </p>
 </div>
