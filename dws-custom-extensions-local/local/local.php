b285d40acce032b38b610a0f9a4f1a266edb14a8
Antonius Hegyes <a.hegyes@deep-web-solutions.de>
2019-12-07 16:59:27 +0100

    Bug fix ... missing function

--- dws-custom-extensions-local/local/local.php
+++ dws-custom-extensions-local/local/local.php
@@ -66,5 +66,34 @@ final class DWS_Local extends DWS_Singleton {
         );
     }
 
+    /**
+     * MU plugins inside directories are not returned in `get_mu_plugins`.
+     * This filter modifies the array obtained from Wordpress when Loco Translate grabs it.
+     *
+     * Note that this filter only runs once per script execution, because the value is cached.
+     * Define the function *before* Loco Translate plugin is even included by WP.
+     *
+     * @since   1.0.0
+     * @version 1.0.0
+     *
+     * @param   array   $plugins    The plugins that have already been successfully registered with Loco Translate.
+     *
+     * @return  array   The plugins registered with Loco Translate including the pseudo-plugin of the local extensions.
+     */
+    public function register_with_loco_translate_plugin(array $plugins) {
+        // we know the plugin by this handle, even if WordPress doesn't
+        $handle = 'dws-custom-extensions-local/local/local.php';
+
+        // fetch the plugin's meta data from the would-be plugin file
+        $data = get_plugin_data(trailingslashit(WPMU_PLUGIN_DIR) . $handle);
+
+        // extra requirement of Loco - $handle must be resolvable to full path
+        $data['basedir'] = WPMU_PLUGIN_DIR;
+
+        // add to array and return back to Loco Translate
+        $plugins[$handle] = $data;
+        return $plugins;
+    }
+
     //endregion
 }
